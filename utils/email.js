2f24996962fe0958c1e7f2709c3bbc639a55c8a8
casolanki <uvchsolanki@gmail.com>
2023-03-07 16:47:54 +0530

    modified packagejosn added sqllite and nodemon

--- utils/email.js
+++ utils/email.js
@@ -32,35 +32,5 @@ module.exports = {
              })
 
                 completion()
-
           }
-
-    
-    // sendEmail: function(emailInfo){
-
-    //     return new Promise(function(resolve, reject){
-    //         var from_email = new helper.Email('uvchsolanki@gmail.com')
-    //         var to_email = new helper.Email(emailInfo.recipient)
-    //         var subject = emailInfo.subject
-    //         var content = new helper.Content('text/html', emailInfo.content)
-    //         var mail = new helper.Mail(from_email, subject, to_email, content)
-            
-    //         var sg = require('sendgrid')(apikey)
-    //         var request = sg.emptyRequest({
-    //             method: 'POST',
-    //             path: '/v3/mail/send',
-    //             body: mail.toJSON(),
-    //         })
-          
-    //         sg.API(request, function(error, response) {
-    //             if(error)
-    //             {
-    //                 reject(error)
-    //                 return
-    //             }
-    //               resolve(response)
-    //             })
-    //          })
-    //      }
-    
 }
